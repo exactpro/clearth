3c347f40f35bde3881709fc21d4b4016edd834be
pavel.tropinov <pavel.tropinov@exactprosystems.com>
2019-10-08 19:57:36 +0300

    Feature #55021. [Core] Reader and writer for table-like data from fixed length files
    Change-Id: I667649dec03f34c75e4a2fedafc1419530dacc9c

---
+++ clearth-core/src/main/java/com/exactprosystems/clearth/utils/tabledata/readers/FlatFileReader.java
@@ -0,0 +1,47 @@
+/******************************************************************************
+ * Copyright 2009-2019 Exactpro Systems Limited
+ * https://www.exactpro.com
+ * Build Software to Test Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.exactprosystems.clearth.utils.tabledata.readers;
+
+import com.exactprosystems.clearth.connectivity.flat.FlatMessageDesc;
+import com.exactprosystems.clearth.utils.tabledata.RowsListFactory;
+import com.exactprosystems.clearth.utils.tabledata.StringTableData;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Set;
+
+public class FlatFileReader extends AbstractFlatFileReader<StringTableData>
+{
+	public FlatFileReader(File file, FlatMessageDesc messageDesc) throws IOException
+	{
+		super(file, messageDesc);
+	}
+	
+	public FlatFileReader(Reader reader, FlatMessageDesc messageDesc)
+	{
+		super(reader, messageDesc);
+	}
+	
+	@Override
+	protected StringTableData createTableData(Set<String> header, RowsListFactory<String, String> rowsListFactory)
+	{
+		return new StringTableData(header, rowsListFactory);
+	}
+}
---
+++ clearth-core/src/main/java/com/exactprosystems/clearth/utils/tabledata/readers/IndexedFlatFileDataReader.java
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * Copyright 2009-2019 Exactpro Systems Limited
+ * https://www.exactpro.com
+ * Build Software to Test Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.exactprosystems.clearth.utils.tabledata.readers;
+
+import com.exactprosystems.clearth.connectivity.flat.FlatMessageDesc;
+import com.exactprosystems.clearth.utils.tabledata.IndexedTableData;
+import com.exactprosystems.clearth.utils.tabledata.RowsListFactory;
+import com.exactprosystems.clearth.utils.tabledata.TableRowMatcher;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Set;
+
+public class IndexedFlatFileDataReader<C> extends AbstractFlatFileReader<IndexedTableData<String, String, C>>
+{
+	protected TableRowMatcher<String, String, C> rowMatcher;
+	
+	public IndexedFlatFileDataReader(File file, FlatMessageDesc messageDesc, TableRowMatcher<String, String, C> rowMatcher) throws IOException
+	{
+		super(file, messageDesc);
+		this.rowMatcher = rowMatcher;
+	}
+	
+	public IndexedFlatFileDataReader(Reader reader, FlatMessageDesc messageDesc, TableRowMatcher<String, String, C> rowMatcher)
+	{
+		super(reader, messageDesc);
+		this.rowMatcher = rowMatcher;
+	}
+	
+	@Override
+	protected IndexedTableData<String, String, C> createTableData(Set<String> header, RowsListFactory<String, String> rowsListFactory)
+	{
+		return new IndexedTableData<>(header, rowMatcher, rowsListFactory);
+	}
+}
