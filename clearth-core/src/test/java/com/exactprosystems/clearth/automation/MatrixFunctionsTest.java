6c941ec103e4d408c4ca31eff43c3c55a8464aee
nikolay.bondarev <nikolay.bondarev@exactprosystems.com>
2020-05-27 16:06:49 +0400

    Feature #66512. Added "Text functions" tests.
    Change-Id: I2cb169da6b339c0553badb109b4f31068e76eb38

--- clearth-core/src/test/java/com/exactprosystems/clearth/automation/MatrixFunctionsTest.java
+++ clearth-core/src/test/java/com/exactprosystems/clearth/automation/MatrixFunctionsTest.java
@@ -34,16 +34,16 @@ import static org.testng.Assert.*;
 public class MatrixFunctionsTest
 {
 	private static final String DD_MM_YY_PATTERN = "ddMMyy";
-	
+
 	private volatile MatrixFunctions functionsWithHolidays;
-	
-	
+
+
 	@BeforeClass
 	void prepare()
 	{
 		initFunctions();
 	}
-	
+
 	private void initFunctions()
 	{
 		Date businessDay = new GregorianCalendar(2019, Calendar.APRIL, 30).getTime();
@@ -55,16 +55,16 @@ public class MatrixFunctionsTest
 		holidays.put("20190507", true);
 		holidays.put("20190509", true);
 		holidays.put("20190510", true);
-		
-		functionsWithHolidays = new MatrixFunctions(holidays, 
-		                                            businessDay, 
-		                                            null,
-		                                            true, 
-		                                            null);
+
+		functionsWithHolidays = new MatrixFunctions(holidays,
+				businessDay,
+				null,
+				true,
+				null);
+
 	}
-	
-	
-	
+
+
 	@DataProvider(name = "time")
 	Object[][] createDataForTime()
 	{
@@ -77,16 +77,126 @@ public class MatrixFunctionsTest
 						{3, 0, 0, "130519"}
 				};
 	}
-	
-	
+
 	@Test(dataProvider = "time", invocationCount = 100, threadPoolSize = 10)
 	void checkTime(int dayOffset, int monthOffset, int yearOffset, String expectedDate) throws FunctionException
 	{
 		long timestamp = functionsWithHolidays.time(dayOffset, monthOffset, yearOffset);
-		
+
 		SimpleDateFormat format = new SimpleDateFormat(DD_MM_YY_PATTERN);
 		String actualDate = format.format(timestamp);
-		
+
 		assertEquals(actualDate, expectedDate);
 	}
+
+	@DataProvider(name = "trim")
+	Object[][] createDataForTrimLeft()
+	{
+		return new Object[][]
+				{
+						// length (for trimleft) or offset (for trimright), string to trim, expected string
+						{7, "abcdef", "abcdef"},
+						{6, "abcdef", "abcdef"},
+						{0, "abcdef", ""},
+						{6, "abcdefgjklmnoabcdef", "abcdef"}
+				};
+	}
+
+	@Test(dataProvider = "trim")
+	public void checkTrimleft(int length, String stringToTrim, String expectedString)
+	{
+		String actualString = functionsWithHolidays.trimleft(stringToTrim, length);
+		assertEquals(actualString, expectedString);
+	}
+
+	@Test(dataProvider = "trim")
+	public void checkTrimright(int offset, String stringToTrim, String expectedString)
+	{
+		String actualString = functionsWithHolidays.trimright(stringToTrim, offset);
+		assertEquals(actualString, expectedString);
+	}
+
+	@DataProvider(name = "add-zeros")
+	Object[][] createDataForAddZeros()
+	{
+		return new Object[][]
+				{
+						// string to add zeros, expected string
+						{"123456", "123456"},
+						{"000000", "000000"},
+						{"100.", "100."}
+				};
+	}
+
+	@Test(dataProvider = "add-zeros")
+	public void checkAddZeros(String stringToAddZeros, String expectedString)
+	{
+		String actualString = functionsWithHolidays.addZeros(stringToAddZeros);
+		assertEquals(actualString, expectedString);
+	}
+
+	@DataProvider(name = "add-zeros-with-zeros")
+	Object[][] createDataForAddZerosWithZeros()
+	{
+		return new Object[][]
+				{
+						// string to add zeros, expected string, number of zeros
+						{"123456", "123456", 0},
+						{"123456", "123456", -1},
+						{"000000", "000000.000", 3},
+						{"100.", "100.00", 2}
+				};
+	}
+
+	@Test(dataProvider = "add-zeros-with-zeros")
+	public void checkAddZerosWithZeros(String stringToAddZeros, String expectedString, int numberOfZeros)
+	{
+		String actualString = functionsWithHolidays.addZeros(stringToAddZeros, numberOfZeros);
+		assertEquals(actualString, expectedString);
+	}
+
+	@DataProvider(name = "add-zeros-with-delimiter")
+	Object[][] createDataForAddZerosWithDelimiter()
+	{
+		return new Object[][]
+				{
+						// string to add zeros, expected string, number of zeros, delimiter
+						{"123456", "123456", 0, "."},
+						{"123456", "123456", -1, "."},
+						{"000000", "000000.000", 3, "."},
+						{"00000", "00000/00", 2, "/"},
+						{"100.", "100.00", 2, "."}
+				};
+	}
+
+	@Test(dataProvider = "add-zeros-with-delimiter")
+	public void checkAddZerosWithDelimiter(String stringToAddZeros, String expectedString, int numberOfZeros,
+	                                       String delimiter)
+	{
+		String actualString = functionsWithHolidays.addZeros(stringToAddZeros, numberOfZeros, delimiter);
+		assertEquals(actualString, expectedString);
+	}
+
+	@DataProvider(name = "trim-zeros")
+	Object[][] createDataForTrimZeros()
+	{
+		return new Object[][]
+				{
+						// string to trim, expected string
+						{"123456000", "123456000"},
+						{"123456.000", "123456"},
+						{"12345.6000", "12345.6"},
+						{"12345.6", "12345.6"},
+						{"12345.6070", "12345.607"},
+						{"12345.", "12345"}
+				};
+	}
+
+	@Test(dataProvider = "trim-zeros")
+	public void checkTrimZeros(String stringToTrim, String expectedString)
+	{
+		String actualString = functionsWithHolidays.trimZeros(stringToTrim);
+		assertEquals(actualString, expectedString);
+	}
+
 }
