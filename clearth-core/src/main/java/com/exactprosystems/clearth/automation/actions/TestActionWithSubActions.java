5bac6e355e37878569e1b4e3fc589648c9d543d9
vladimir.panarin <vladimir.panarin@exactprosystems.com>
2023-02-13 15:51:39 +0400

    Feature #83224. [Core] Copyright update
    Change-Id: I8b5f4295cdad8ae596f0709e2dc4bcafc78ab290

--- clearth-core/src/main/java/com/exactprosystems/clearth/automation/actions/TestActionWithSubActions.java
+++ clearth-core/src/main/java/com/exactprosystems/clearth/automation/actions/TestActionWithSubActions.java
@@ -1,12 +1,21 @@
 /******************************************************************************
- * Copyright (c) 2009-2020, Exactpro Systems LLC
- * www.exactpro.com
+ * Copyright 2009-2023 Exactpro Systems Limited
+ * https://www.exactpro.com
  * Build Software to Test Software
  *
- * All rights reserved.
- * This is unpublished, licensed software, confidential and proprietary 
- * information which is the property of Exactpro Systems LLC or its licensors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
+
 package com.exactprosystems.clearth.automation.actions;
 
 import com.exactprosystems.clearth.automation.*;
