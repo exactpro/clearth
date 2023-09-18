27383ee21269033154441724e16becc53c2cd28e
elena.pakhomova <elena.pakhomova@exactprosystems.com>
2023-09-18 15:35:47 +0000

    Bug #84598. [Core] DbConnection.check() doesn't show errors nor success while actually exception occurred
    Change-Id: I5ea9b3774f7a9358bc52df6e445c317978f62196

--- clearth-core/src/main/java/com/exactprosystems/clearth/connectivity/db/DbConnection.java
+++ clearth-core/src/main/java/com/exactprosystems/clearth/connectivity/db/DbConnection.java
@@ -29,16 +29,15 @@ import org.apache.commons.lang.StringUtils;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 @SettingsClass(DbConnectionSettings.class)
 public class DbConnection extends BasicClearThConnection implements ClearThCheckableConnection
 {
+	private static final String EXCEPTION_MESSAGE = "Could not start connection ";
+
 	public DbConnection () {}
 
 	@Override
@@ -49,30 +48,22 @@ public class DbConnection extends BasicClearThConnection implements ClearThCheck
 
 	public Connection getConnection() throws ConnectivityException, SettingsException
 	{
-		if(StringUtils.isBlank(getSettings().getJdbcUrl()))
-			throw new SettingsException("Could not start connection '" + name + "'. URL is empty");
+		if (StringUtils.isBlank(getSettings().getJdbcUrl()))
+			throw new SettingsException(EXCEPTION_MESSAGE + "'" + name + "'" + ". URL is empty");
 
-		Connection connection;
-		try
-		{
-			connection = getConnectionFromDriverManager();
-		}
-		catch (SQLException e)
-		{
-			throw new ConnectionException("Could not start connection '" + name + "'", e);
-		}
+		Connection connection = getConnectionFromDriverManager();
 
 		String query = getSettings().getInitializationQuery();
 
-		if(!StringUtils.isBlank(query))
+		if (!StringUtils.isBlank(query))
 		{
 			try (PreparedStatement preparedStatement = connection.prepareStatement(query))
 			{
 				preparedStatement.execute();
 			}
-			catch (SQLException e)
+			catch (Exception e)
 			{
-				throw new ConnectionException("Error occurred while executing initialization query",e);
+				throw new ConnectionException("Error occurred while executing initialization query", e);
 			}
 		}
 		return connection;
@@ -89,16 +80,23 @@ public class DbConnection extends BasicClearThConnection implements ClearThCheck
 		}
 	}
 
-	protected Connection getConnectionFromDriverManager() throws SQLException
+	protected Connection getConnectionFromDriverManager() throws ConnectionException
 	{
 		DbConnectionSettings conSettings = getSettings();
 		String url = conSettings.getJdbcUrl(),
 			username = conSettings.getUsername(),
 			password = conSettings.getPassword();
 
-		if (StringUtils.isBlank(username))
-			return DriverManager.getConnection(url);
-		else
-			return DriverManager.getConnection(url, username, password);
+		try
+		{
+			if (StringUtils.isBlank(username))
+				return DriverManager.getConnection(url);
+			else
+				return DriverManager.getConnection(url, username, password);
+		}
+		catch (Exception | NoClassDefFoundError e)
+		{
+			throw new ConnectionException(EXCEPTION_MESSAGE + "'" + name + "'", e);
+		}
 	}
 }
